a99fbfbc5b004fbc0680718b307a557ca2bcb9f5
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-06-07 19:50:25 +0200

    Tradotto

--- embed/embed.py
+++ embed/embed.py
@@ -1,3 +1,10 @@
+"""
+Plugin di Embedder per VincyBot07.
+
+Tradotto da Ergastolator1.
+Tutti i diritti riservati.
+"""
+
 import re
 from datetime import datetime
 
@@ -10,9 +17,9 @@ from core.checks import has_permissions
 from core.models import PermissionLevel
 
 
-class Embed(Cog):
-    """Easily make embeds for a nicer look.
-    More info: [click here](https://github.com/papiersnipper/modmail-plugins/tree/master/embedder)
+class Embedder(Cog):
+    """Crea facilmente embed per un look più fantastico.
+    Più informazioni: [click here](https://github.com/Ergastolator1/fhmail-plugins/tree/master/embedder)
     """
 
     def __init__(self, bot: Bot) -> None:
@@ -22,20 +29,20 @@ class Embed(Cog):
     @group(name="embedder", aliases=["embed"], invoke_without_command=True)
     @has_permissions(PermissionLevel.MODERATOR)
     async def embedder(self, ctx: Context) -> None:
-        """Easily make embeds for a nicer presence."""
+        """Crea facilmente embed per una presenza più fantastica."""
         await ctx.send_help(ctx.command)
 
     @embedder.command(name="color", aliases=["colour"])
     @has_permissions(PermissionLevel.MODERATOR)
     async def color(self, ctx: Context, colorcode: str) -> None:
-        """Save an embed colorcode for future embeds."""
+        """Salva un codice di colore per futuri embed."""
         is_valid = re.search(r"^#(?:[0-9a-fA-F]{3}){1,2}$", colorcode)
 
         if not is_valid:
             embed = Embed(
                 title="Embedder",
-                url="https://github.com/papiersnipper/modmail-plugins/blob/master/embedder",
-                description="Please enter a **valid** [hex code](https://htmlcolorcodes.com/color-picker)",
+                url="https://github.com/Ergastolator1/fhmail-plugins/blob/master/embedder",
+                description="Per favore inserisci un valido [codice hex](https://htmlcolorcodes.com/color-picker)",
                 color=self.bot.main_color,
             )
 
@@ -49,8 +56,8 @@ class Embed(Cog):
 
         embed = Embed(
             title="Embedder",
-            url="https://github.com/papiersnipper/modmail-plugins/blob/master/embedder",
-            description="This color will be used for every future embed.",
+            url="https://github.com/Ergastolator1/fhmail-plugins/blob/master/embedder",
+            description="Questo colore verrà usato per i futuri embed.",
             color=Color(int(colorcode, 0)),
         )
 
@@ -59,7 +66,7 @@ class Embed(Cog):
     @embedder.command(name="send", aliases=["make"])
     @has_permissions(PermissionLevel.MODERATOR)
     async def send(self, ctx: Context, title: str, *, message: str) -> None:
-        """Send an embed."""
+        """Invia un embed."""
         try:
             colorcode = (await self.db.find_one({"_id": "embedcolor-config"}))["colorcode"]
         except (KeyError, TypeError):
@@ -88,8 +95,8 @@ class Embed(Cog):
 
             embed = Embed(
                 title="Embedder",
-                url="https://github.com/papiersnipper/modmail-plugins/blob/master/embedder",
-                description="You can only use one image per embed.",
+                url="https://github.com/Ergastolator1/fhmail-plugins/blob/master/embedder",
+                description="Puoi solo usare un immagine per embed.",
                 color=self.bot.main_color,
             )
 
@@ -101,5 +108,5 @@ class Embed(Cog):
 
 
 def setup(bot: Bot) -> None:
-    """Bot cog load."""
-    bot.add_cog(Embed(bot))
+    """Caricamento bot cog."""
+    bot.add_cog(Embedder(bot))
