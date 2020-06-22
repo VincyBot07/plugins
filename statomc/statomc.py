a18cd060e219b1b3c253d7503b295805a1be73c5
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-06-22 08:26:57 +0200

    Update statomc.py

--- statomc/statomc.py
+++ statomc/statomc.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import requests
+import json
+import os
 
 class StatoMC(commands.Cog):
       def __init__(self, bot):
@@ -8,13 +11,25 @@ class StatoMC(commands.Cog):
       @commands.command(aliases=["stato"])
       async def statomc(self, ctx):
             """Mostra lo stato del server Minecraft."""
-            response = await self.bot.session.get("http://statomc.vincysuper07.cf/api.php")
-            status = (await response.content.readline()).decode('UTF-8')
-            embed = discord.Embed(title = "Server Minecraft: mc.Vincysuper07.cf", description = f"Al momento il server è {status}")
-            if status == "OFFLINE.":
-                embed.color = discord.Color.red()
+            mc_url = os.getenv("MINECRAFT_SERVER")
+            response = requests.get(f'https://api.mcsrvstat.us/2/{mc_url')
+            status = json.loads(rsp.text)
+            #print(load["online"])
+            online = status["online"]
+            players = status["players"]["online"]
+            max = status["players"]["max"]
+            version = status["version"]
+            #embed = discord.Embed(title = "Server Minecraft: {mc_url}", description = f"Al momento il server è {status}")
+            if online == True:
+                if players == "1":
+                    e = discord.Embed(title = "Server Minecraft: {mc_url}", description = f"Al momento il server è **online**. **1** persona sta giocando sul massimo di {max}.", color = discord.Color.green())
+                else:
+                    e = discord.Embed(title = "Server Minecraft: {mc_url}", description = f"Al momento il server è **online**. **{players}** persone stanno giocando sul massimo di {max}.")
+                if players == "0":
+                    e = discord.Embed(title = "Server Minecraft: {mc_url}", description = f"Al momento il server è **online**.")
+                e.set_footer(text="La versione è {version}.")
             else:
-                embed.color = discord.Color.green()
+                e = discord.Embed(title = "Server Minecraft: {mc_url}", description = f"Al momento il server è **offline**.", color=discord.Color.red()).set_footer(text="Non posso sapere la versione, dato che il server è offline.")
             await ctx.send(embed=embed)
 
 def setup(bot):
