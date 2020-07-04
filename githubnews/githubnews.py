e35504ef53965d9d87512fada6686d3302af8030
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-07-04 12:03:58 +0200

    Update githubnews.py

--- githubnews/githubnews.py
+++ githubnews/githubnews.py
@@ -5,3 +5,9 @@ from discord.ext import commands
 class GitHubNews(commands.Cog, name="GitHub News")
   def __init(self, bot):
     self.bot = bot
+
+    
+    
+    
+def setup(bot):
+  bot.add_cog(GitHubNews(bot))
