89c3164d942c8e5404af78a54ccec7c20ea9a819
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-07-04 12:42:29 +0200

    Update githubnews.py

--- githubnews/githubnews.py
+++ githubnews/githubnews.py
@@ -1,13 +1,23 @@
 import json
+import random
 import discord
 from discord.ext import commands
+colors = [0x6bc7ea, 0xc73650, 0xfdf95b]
 
 class GitHubNews(commands.Cog, name="GitHub News"):
-  def __init(self, bot):
-    self.bot = bot
+    def __init(self, bot):
+        self.bot = bot
+        self.request = await bot.session.get("https://api.github.com/repos/VincyBot07/VincyBot07/releases/latest")
+        self.response = await request.content.readline()
+        self.resp = json.loads(response)
+        self.colors = [0x6bc7ea, 0xc73650, 0xfdf95b]
+        self.color = random.choice(colors)
+    #
+    @commands.command()
+    async def qpowieurtyturiewqop(self,ctx):
+        e = discord.Embed(title="testù", description=f"versione: {self.resp["tag_name"}")
 
-    
-    
-    
+
+
 def setup(bot):
-  bot.add_cog(GitHubNews(bot))
+    bot.add_cog(GitHubNews(bot))
